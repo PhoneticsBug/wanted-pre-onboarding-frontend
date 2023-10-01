12c78126a6197b0429710a0de6f04e96c7876878
phoneticsbug <swwe1000@gmail.com>
2023-10-01 15:53:11 +0900

    init: theme, not working through app.js(hold)

--- git-issues/src/App.js
+++ git-issues/src/App.js
@@ -8,17 +8,19 @@ import MainPage from './pages/index.js'
 // components
 import Header from './components/header.js'
 import Footer from './components/footer.js'
+import { ThemeProvider } from "./context/themeProvider";
+import { GlobalStyle } from "./theme/GlobalStyle";
+// theme이 작동하지 않으므로 현재는 사용 X
 
 const App = () => {
-  const [isValid, setIsValid] = useState("");
 
   return (
     <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path="/" element={<MainPage/>} />
-        {/* <Route path="" element={}/> */}
-      </Routes>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<MainPage/>} />
+          {/* <Route path="" element={}/> */}
+        </Routes>
       <Footer/>
     </BrowserRouter>
   );
